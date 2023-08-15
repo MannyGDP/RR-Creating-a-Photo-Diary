3a12fcde3c71dba7b85ddd5f48c325523000f107
Manny Delgado <mannygdp@gmail.com>
2023-08-14 22:06:30 -0700

    Started

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import { koalas } from './koalas'
 let activeKoala = null
 
 // Changes the active koala, then re-renders the page to display that koala
@@ -14,7 +14,7 @@ let render = () => {
     if(activeKoala){
         newPage = renderActiveKoala(activeKoala)
     } else {
-        newPage = renderKoalaList(koalas)
+        newPage = renderKoalaList( koalas)
     }
     newPage.classList.add('koala-container')
     oldPage.replaceWith(newPage)
